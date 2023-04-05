006c169c9471b878c1d70dfb34b9cf3e3470d5d7
HaIOUPAY <tran.habk0605@gmail.com>
2023-04-05 16:25:58 +0700

    catalog/product module

---
+++ eShopSolution.Domain/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,15 @@
+﻿using eShopSolution.Domain.Catalog.Dtos;
+using eShopSolution.Domain.Catalog.Products.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Domain.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        public PagedViewModel<ProductViewModel> GetAllByCategoryId(int categoryId, int pageIndex, int pageSize);
+    }
+}
