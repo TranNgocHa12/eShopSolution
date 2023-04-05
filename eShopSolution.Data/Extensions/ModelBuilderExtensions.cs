f9728bfec71b17339861a78348056c70c8fb4f39
HaIOUPAY <tran.habk0605@gmail.com>
2023-04-05 15:25:35 +0700

    identity seeding

--- eShopSolution.Data/Extensions/ModelBuilderExtensions.cs
+++ eShopSolution.Data/Extensions/ModelBuilderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using eShopSolution.Data.Entities;
 using eShopSolution.Data.Enums;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace eShopSolution.Data.Extensions
@@ -81,38 +82,38 @@ namespace eShopSolution.Data.Extensions
                 new ProductInCategory() { ProductId = 1, CategoryId = 1 }
                 );
 
-            //// any guid
-            //var roleId = new Guid("8D04DCE2-969A-435D-BBA4-DF3F325983DC");
-            //var adminId = new Guid("69BD714F-9576-45BA-B5B7-F00649BE00DE");
-            //modelBuilder.Entity<AppRole>().HasData(new AppRole
-            //{
-            //    Id = roleId,
-            //    Name = "admin",
-            //    NormalizedName = "admin",
-            //    Description = "Administrator role"
-            //});
+            // any guid
+            var roleId = new Guid("8D04DCE2-969A-435D-BBA4-DF3F325983DC");
+            var adminId = new Guid("69BD714F-9576-45BA-B5B7-F00649BE00DE");
+            modelBuilder.Entity<AppRole>().HasData(new AppRole
+            {
+                Id = roleId,
+                Name = "admin",
+                NormalizedName = "admin",
+                Description = "Administrator role"
+            });
 
-            //var hasher = new PasswordHasher<AppUser>();
-            //modelBuilder.Entity<AppUser>().HasData(new AppUser
-            //{
-            //    Id = adminId,
-            //    UserName = "admin",
-            //    NormalizedUserName = "admin",
-            //    Email = "tedu.international@gmail.com",
-            //    NormalizedEmail = "tedu.international@gmail.com",
-            //    EmailConfirmed = true,
-            //    PasswordHash = hasher.HashPassword(null, "Abcd1234$"),
-            //    SecurityStamp = string.Empty,
-            //    FirstName = "Toan",
-            //    LastName = "Bach",
-            //    Dob = new DateTime(2020, 01, 31)
-            //});
+            var hasher = new PasswordHasher<AppUser>();
+            modelBuilder.Entity<AppUser>().HasData(new AppUser
+            {
+                Id = adminId,
+                UserName = "admin",
+                NormalizedUserName = "admin",
+                Email = "tran.habk0605@gmail.com",
+                NormalizedEmail = "tran.habk0605@gmail.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "123456#"),
+                SecurityStamp = string.Empty,
+                FirstName = "Ngoc Ha",
+                LastName = "Tran",
+                Dob = new DateTime(2020, 01, 31)
+            });
 
-            //modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(new IdentityUserRole<Guid>
-            //{
-            //    RoleId = roleId,
-            //    UserId = adminId
-            //});
+            modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(new IdentityUserRole<Guid>
+            {
+                RoleId = roleId,
+                UserId = adminId
+            });
 
             //modelBuilder.Entity<Slide>().HasData(
             //  new Slide() { Id = 1, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 1, Url = "#", Image = "/themes/images/carousel/1.png", Status = Status.Active },
---
+++ eShopSolution.Data/Migrations/20230405080902_identity.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShopSolution.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_AppUsers_AppUserId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AppUsers_AppUserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AppUsers_AppUserId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_AppUserId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AppUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2023, 4, 5, 15, 9, 2, 40, DateTimeKind.Local).AddTicks(3402),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2023, 4, 5, 14, 55, 59, 858, DateTimeKind.Local).AddTicks(913));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AppUserId",
+                table: "Carts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 5, 15, 9, 2, 44, DateTimeKind.Local).AddTicks(4866));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_AppUsers_AppUserId",
+                table: "Carts",
+                column: "AppUserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AppUsers_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AppUsers_UserId",
+                table: "Transactions",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_AppUsers_AppUserId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AppUsers_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AppUsers_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AppUserId",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2023, 4, 5, 14, 55, 59, 858, DateTimeKind.Local).AddTicks(913),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2023, 4, 5, 15, 9, 2, 40, DateTimeKind.Local).AddTicks(3402));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AppUserId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AppUserId",
+                table: "Carts",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 5, 14, 55, 59, 859, DateTimeKind.Local).AddTicks(7927));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_AppUserId",
+                table: "Transactions",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AppUserId",
+                table: "Orders",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_AppUsers_AppUserId",
+                table: "Carts",
+                column: "AppUserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AppUsers_AppUserId",
+                table: "Orders",
+                column: "AppUserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AppUsers_AppUserId",
+                table: "Transactions",
+                column: "AppUserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id");
+        }
+    }
+}
